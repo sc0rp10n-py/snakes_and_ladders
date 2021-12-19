9349ac4f4db3c2daded63b37f36fc79482fe5bf1
sc0rp10n-w11lappy <pragyan52yadav@gmail.com>
2021-12-19 22:08:21 +0530

    basic stuff and icon

--- src/main/java/project/ap/snakes_and_ladders/HelloApplication.java
+++ src/main/java/project/ap/snakes_and_ladders/HelloApplication.java
@@ -3,6 +3,7 @@ package project.ap.snakes_and_ladders;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,9 +11,11 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        Image icon = new Image(String.valueOf(HelloApplication.class.getResource("icon.png")));
+        stage.getIcons().add(icon);
+        stage.setTitle("Snakes and Ladders");
         stage.setScene(scene);
         stage.show();
     }
