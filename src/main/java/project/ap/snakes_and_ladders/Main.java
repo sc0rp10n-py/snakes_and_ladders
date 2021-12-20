378f42d3ba89ca2e2c44c1a710ebccb31616e985
Pragyan_Desktop <pragyan52yadav@gmail.com>
2021-12-20 05:34:24 +0530

    scenes made, and basic moving

--- src/main/java/project/ap/snakes_and_ladders/Main.java
+++ src/main/java/project/ap/snakes_and_ladders/Main.java
@@ -8,12 +8,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class Main extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("welcome.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(Main.class.getResource("welcome.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
-        Image icon = new Image(String.valueOf(HelloApplication.class.getResource("icon.png")));
+        Image icon = new Image(String.valueOf(Main.class.getResource("icon.png")));
         stage.getIcons().add(icon);
         stage.setTitle("Snakes and Ladders");
         stage.setScene(scene);
